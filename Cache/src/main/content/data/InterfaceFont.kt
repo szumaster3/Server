830a737f4e08496771b70ffc18075ed91fd3aa1f
szumaster3 <szumaster2009@protonmail.com>
2026-03-15 21:42:28 +0100

    Add interface_font_examples.png

--- Cache/src/main/content/data/InterfaceFont.kt
+++ Cache/src/main/content/data/InterfaceFont.kt
@@ -1,9 +1,14 @@
 package content.data
 
+/**
+ * @see <a href="References/interface_font_examples.png">References/interface_font_examples.png</a>
+ */
 object InterfaceFont {
     const val P11_FULL = 494
     const val P12_FULL = 495
     const val B12_FULL = 496
     const val Q8_FULL  = 497
     const val QUILL_OBLIQUE_LARGE = 645
+    const val QUILL_CAPS_LARGE = 646
+
 }
